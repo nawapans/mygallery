991496dc5d7c6bbc6c7538210785f8073d0e52a4
nawapans <nawapan.suntorachai@students.iaac.net>
2021-03-19 05:45:43 +0700

    Update script.js

--- 05-Pavilion/script.js
+++ 05-Pavilion/script.js
@@ -4,6 +4,7 @@ import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@0.126.0/exampl
 import { TransformControls } from 'https://cdn.jsdelivr.net/npm/three@0.126.0/examples/jsm/controls/TransformControls.js'
 import { Rhino3dmLoader } from 'https://cdn.jsdelivr.net/npm/three@0.126.0/examples/jsm/loaders/3DMLoader.js'
 import rhino3dm from 'https://cdn.jsdelivr.net/npm/rhino3dm@0.15.0-beta/rhino3dm.module.js'
+import { RhinoCompute } from 'https://cdn.jsdelivr.net/npm/compute-rhino3d@0.13.0-beta/compute.rhino3d.module.js'
 
 // set up loader for converting the results to threejs
 const loader = new Rhino3dmLoader()
